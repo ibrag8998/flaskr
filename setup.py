226feaa02e2bae029624f3c3b98c0fc2ca18d2a9
ibrag8998 <ibragdzh@gmail.com>
2020-05-03 00:25:10 +0300

    added setup functionality

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=['flaskr'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
+
